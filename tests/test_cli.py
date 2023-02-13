26cdca0179773ba459470128c51ecb75e1e3eee3
mkb79 <mkb79@hackitall.de>
2023-02-13 23:17:07 +0100

    test: update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@
 import pytest
 from click.testing import CliRunner
 
-from audible_cli import __main__
+from audible_cli import cli
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(cli.cli)
     assert result.exit_code == 0
