8347bcc8754e463ed37b6519d721fd851b372916
mkb79 <mkb79@hackitall.de>
2023-02-19 12:52:46 +0100

    style(cmd_library): restyle library command

--- src/audible_cli/cmds/cmd_library.py
+++ src/audible_cli/cmds/cmd_library.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 import pathlib
 
@@ -12,7 +11,6 @@ from ..decorators import (
     pass_session,
     start_date_option,
     timeout_option,
-    wrap_async,
 )
 from ..models import Library
 from ..utils import export_to_csv
@@ -20,7 +18,7 @@ from ..utils import export_to_csv
 
 @click.group("library")
 def cli():
-    """interact with library"""
+    """Interact with library."""
 
 
 async def _get_library(session, client, resolve_podcasts):
@@ -77,35 +75,46 @@ async def _get_library(session, client, resolve_podcasts):
 @pass_session
 @pass_client
 async def export_library(session, client, **params):
-    """export library"""
+    """Export library."""
 
-    @wrap_async
     def _prepare_item(item):
+        keys_with_raw_values = (
+            "asin",
+            "title",
+            "subtitle",
+            "extended_product_description",
+            "runtime_length_min",
+            "is_finished",
+            "percent_complete",
+            "release_date",
+            "purchase_date",
+        )
         data_row = {}
         for key in item:
-            v = getattr(item, key)
-            if v is None:
-                pass
-            elif key in keys_with_raw_values:
-                data_row[key] = v
+            value = getattr(item, key)
+            if value is None:
+                continue
+
+            if key in keys_with_raw_values:
+                data_row[key] = value
             elif key in ("authors", "narrators"):
-                data_row[key] = ", ".join([i["name"] for i in v])
+                data_row[key] = ", ".join([i["name"] for i in value])
             elif key == "series":
-                data_row["series_title"] = v[0]["title"]
-                data_row["series_sequence"] = v[0]["sequence"]
+                data_row["series_title"] = value[0]["title"]
+                data_row["series_sequence"] = value[0]["sequence"]
             elif key == "rating":
-                overall_distributing = v.get("overall_distribution") or {}
+                overall_distributing = value.get("overall_distribution") or {}
                 data_row["rating"] = overall_distributing.get(
                     "display_average_rating", "-"
                 )
                 data_row["num_ratings"] = overall_distributing.get("num_ratings", "-")
             elif key == "library_status":
-                data_row["date_added"] = v["date_added"]
+                data_row["date_added"] = value["date_added"]
             elif key == "product_images":
-                data_row["cover_url"] = v.get("500", "-")
+                data_row["cover_url"] = value.get("500", "-")
             elif key == "category_ladders":
                 genres = []
-                for genre in v:
+                for genre in value:
                     for ladder in genre["ladder"]:
                         genres.append(ladder["name"])
                 data_row["genres"] = ", ".join(genres)
@@ -120,29 +129,12 @@ async def export_library(session, client, **params):
 
     resolve_podcasts = params.get("resolve_podcasts")
     library = await _get_library(session, client, resolve_podcasts)
-
-    keys_with_raw_values = (
-        "asin",
-        "title",
-        "subtitle",
-        "extended_product_description",
-        "runtime_length_min",
-        "is_finished",
-        "percent_complete",
-        "release_date",
-        "purchase_date",
+    prepared_library = list(
+        filter(lambda x: x is not None, map(_prepare_item, library))
     )
-
-    prepared_library = await asyncio.gather(*[_prepare_item(i) for i in library])
-    prepared_library = [i for i in prepared_library if i is not None]
     prepared_library.sort(key=lambda x: x["asin"])
 
     if output_format in ("tsv", "csv"):
-        if output_format == "csv":
-            dialect = "excel"
-        else:
-            dialect = "excel-tab"
-
         headers = (
             "asin",
             "title",
@@ -163,10 +155,10 @@ async def export_library(session, client, **params):
             "cover_url",
             "purchase_date",
         )
-
+        dialect = "excel" if output_format == "csv" else "excel-tab"
         export_to_csv(output_filename, prepared_library, headers, dialect)
 
-    elif output_format == "json":
+    else:
         data = json.dumps(prepared_library, indent=4)
         output_filename.write_text(data)
 
@@ -182,28 +174,20 @@ async def export_library(session, client, **params):
 @pass_session
 @pass_client
 async def list_library(session, client, resolve_podcasts):
-    """list titles in library"""
+    """List library."""
 
-    @wrap_async
     def _prepare_item(item):
         fields = [item.asin]
 
-        authors = ", ".join(
-            sorted(a["name"] for a in item.authors) if item.authors else ""
-        )
-        if authors:
-            fields.append(authors)
+        if item.authors:
+            fields.append(", ".join(sorted(a["name"] for a in item.authors)))
 
-        series = ", ".join(
-            sorted(s["title"] for s in item.series) if item.series else ""
-        )
-        if series:
-            fields.append(series)
+        if item.series:
+            fields.append(", ".join(sorted(s["title"] for s in item.series)))
 
         fields.append(item.title)
         return ": ".join(fields)
 
     library = await _get_library(session, client, resolve_podcasts)
-
-    books = await asyncio.gather(*[_prepare_item(i) for i in library])
-    [echo(i) for i in sorted(books) if len(i) > 0]
+    books = sorted(map(_prepare_item, library))
+    list(map(echo, filter(lambda x: len(x) > 0, books)))
