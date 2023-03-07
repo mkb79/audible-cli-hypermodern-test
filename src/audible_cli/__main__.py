4679b2e27c46c79d0bc4e37dc167ec7df1fb4436
mkb79 <mkb79@hackitall.de>
2023-03-07 16:06:49 +0100

    refactor: __main__.py

--- src/audible_cli/__main__.py
+++ src/audible_cli/__main__.py
@@ -3,9 +3,5 @@ import sys
 from . import cli
 
 
-def main():
-    return sys.exit(cli.main(prog_name="python -m audible_cli"))
-
-
 if __name__ == "__main__":
-    main()
+    sys.exit(cli.main(prog_name="python -m audible_cli"))  # pragma: no cover
